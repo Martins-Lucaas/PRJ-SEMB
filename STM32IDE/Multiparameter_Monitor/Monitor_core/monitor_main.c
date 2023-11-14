68317ac8630097a71d8c3cb36b9482f4425aac72
Lucas_Martins <lucaspmartins14@gmail.com>
2023-11-14 12:01:07 -0300

    PRJ5

--- STM32IDE/Multiparameter_Monitor/Monitor_core/monitor_main.c
+++ STM32IDE/Multiparameter_Monitor/Monitor_core/monitor_main.c
@@ -39,4 +39,44 @@ void loop();
  * */
 
 
-
+/* PWM
+ *
+ * modular um sinal significa alterar a caracteristca de um sinal para outra forma
+ *
+ * FM frequencia modular
+ * banda base 20 a 20kHz
+ *
+ * PWM é um sinal periódico uma onda quadrada com períodos
+ *
+ * Importante para o PWM: Período e Duty cicle
+ *
+ * Implica um circuito de período morto entre a mudança de PWM complementares
+ *
+ * capture compare register vai gerar o ciclo
+ * GERAR UM PWM COM A FREW DE 20KHZ
+ * COM DUTY CICLE DE 50%
+ *
+ * SEMPRE CRIAR UM .H E UM .C PARA O CÓDIGO
+ * CADA SENSOR VAI TER UMA DE CADA
+ * COLOCAR UM MONITOR.C PARA JUNTAR A CHAMADA DE TUDO
+ * CONTROLAR O CICLO DOS SENSORES COM PWM
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
