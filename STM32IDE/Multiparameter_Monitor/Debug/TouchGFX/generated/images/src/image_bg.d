ae5b41dcadadab1aa76fdfe54f9f7ceeff193238
Lucas_Martins <lucaspmartins14@gmail.com>
2023-11-28 11:36:26 -0300

    Final commit

--- STM32IDE/Multiparameter_Monitor/Debug/TouchGFX/generated/images/src/image_bg.d
+++
@@ -1,4 +0,0 @@
-TouchGFX/generated/images/src/image_bg.o: \
- ../TouchGFX/generated/images/src/image_bg.cpp \
- ../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
