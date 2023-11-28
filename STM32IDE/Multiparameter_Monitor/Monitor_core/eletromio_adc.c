29b6326173b0283c8e42e0a163eeba5e12413564
Lucas_Martins <lucaspmartins14@gmail.com>
2023-11-28 02:26:43 -0300

    EMG finalizado

--- STM32IDE/Multiparameter_Monitor/Monitor_core/eletromio_adc.c
+++ STM32IDE/Multiparameter_Monitor/Monitor_core/eletromio_adc.c
@@ -12,22 +12,28 @@
 
 extern TIM_HandleTypeDef hadc1;
 extern UART_HandleTypeDef huart3;
+extern TIM_HandleTypeDef htim2;
 #define ADC_Buffer 4096
+
+
+void HAL_ADC_HalfCpltCallback(ADC_HandleTypeDef* hadc){
+    HAL_GPIO_WritePin(GPIOA, led_adc_Pin|GPIO_PIN_9, GPIO_PIN_SET);
+}
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc){
+    HAL_GPIO_WritePin(GPIOA, led_adc_Pin|GPIO_PIN_9, GPIO_PIN_RESET);
+}
 
 void emg_system(void)
 {
-	uint16_t adc_buf[ADC_Buffer];
-	HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adc_buf, ADC_Buffer);
-	if (HAL_ADC_CompletoCallBack){
-		return adc_buf;
-	}
+    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+    uint16_t adc_buf[ADC_Buffer];
+    HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adc_buf, ADC_Buffer);
 
-}
-void HAL_ADC_MetadeCompletoCallBack(ADC_HandleTypeDef* hadc){
-	HAL_GPIO_WritePin(GPIOA, led_adc_Pin|GPIO_PIN_9, GPIO_PIN_SET);
-
-}
-void HAL_ADC_CompletoCallBack(ADC_HandleTypeDef* hadc){
-	HAL_GPIO_WritePin(GPIOA, led_adc_Pin|GPIO_PIN_9, GPIO_PIN_RESET);
-
+    for (int i = 0; i < ADC_Buffer; i++) {
+        uint8_t data[2];
+        data[0] = (adc_buf[i] & 0xFF00) >> 8;
+        data[1] = adc_buf[i] & 0x00FF;
+        HAL_UART_Transmit(&huart3, data, 2, HAL_MAX_DELAY);
+    }
 }
